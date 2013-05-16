a825742699f3cc301af660c12e5b963c9eeb8221
Ken Sipe <kensipe@gmail.com>
2013-05-16 08:21:33 -0500

    adding google search code and tests

--- src/main/java/com/cmentor/search/google/GoogleSearchService.java
+++ src/main/java/com/cmentor/search/google/GoogleSearchService.java
@@ -2,27 +2,66 @@ package com.cmentor.search.google;
 
 import com.cmentor.search.SearchResultLink;
 import com.cmentor.search.SearchService;
+import com.cmentor.search.google.model.GoogleResults;
+import com.cmentor.search.google.model.GoogleSearchResultLink;
 import com.cmentor.search.google.service.GoogleInternalSearchService;
+import com.cmentor.search.google.service.GoogleListTransformer;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author kensipe
  */
 @Component
-public class GoogleSearchService implements SearchService{
+public class GoogleSearchService implements SearchService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     GoogleInternalSearchService searchService;
+    GoogleJsonResultParser parser = new GoogleJsonResultParser();
 
     @Override
     public List<SearchResultLink> search(String criteria) {
-        return null;
+
+        List<GoogleSearchResultLink> googleLinks = getGoogleSearchResults(criteria);
+        List<SearchResultLink> links = GoogleListTransformer.transform(googleLinks);
+        return links;
+    }
+
+    private List<GoogleSearchResultLink> getGoogleSearchResults(String criteria) {
+
+        String json = null;
+        try {
+            json = searchService.search(criteria);
+        } catch (Exception e) {
+            logger.error("Failure to make http connection", e);
+        }
+
+        return parseJson(json);
+    }
+
+    private List<GoogleSearchResultLink> parseJson(String json) {
+        List<GoogleSearchResultLink> links = null;
+        if (StringUtils.isNotBlank(json)) {
+            try {
+                GoogleResults results = parser.parse(json);
+                links = results.getData().getResults();
+            } catch (IOException e) {
+                logger.error("unable to parse json: {}", json, e);
+
+            }
+        }
+        if (links == null) {
+            links = new ArrayList<GoogleSearchResultLink>();
+        }
+        return links;
     }
 }
---
+++ src/test/groovy/com/cmentor/search/google/GoogleSearchServiceSpec.groovy
@@ -0,0 +1,109 @@
+package com.cmentor.search.google
+
+import com.cmentor.search.google.service.GoogleInternalSearchService
+import spock.lang.Specification
+
+/**
+ * 
+ * @author kensipe
+ */
+class GoogleSearchServiceSpec extends Specification {
+
+    GoogleSearchService searchService = new GoogleSearchService();
+
+        def "no failure on bad json"() {
+
+            when:
+            def links = searchService.parseJson(json)
+
+            then:
+            links.size() == 0
+
+            where:
+            json << ["", "bad json", null]
+        }
+
+        def "json to link results"() {
+            given:
+            def internalSearch = Mock(GoogleInternalSearchService)
+            searchService.searchService = internalSearch
+            internalSearch.search("") >> conjureGoogleJson()
+
+            when:
+            def links = searchService.search("")
+
+            then:
+            links.size() == 3
+            links[0].source == "google"
+
+        }
+
+    def conjureGoogleJson() {
+        """{
+	responseData: {
+		results: [{
+			GsearchResultClass: "GwebSearch",
+			unescapedUrl: "https://twitter.com/kensipe",
+			url: "https://twitter.com/kensipe",
+			visibleUrl: "twitter.com",
+			cacheUrl: "http://www.google.com/search?q=cache:tR4K7L8AIXwJ:twitter.com",
+			title: "<b>Ken Sipe</b> (<b>kensipe</b>) on Twitter",
+			titleNoFormatting: "Ken Sipe (kensipe) on Twitter",
+			content: "The latest from <b>Ken Sipe</b> (@<b>kensipe</b>). Technical Kung Fu Artist. St. Louis, MO."
+		}, {
+			GsearchResultClass: "GwebSearch",
+			unescapedUrl: "http://www.nofluffjuststuff.com/conference/speaker/ken_sipe",
+			url: "http://www.nofluffjuststuff.com/conference/speaker/ken_sipe",
+			visibleUrl: "www.nofluffjuststuff.com",
+			cacheUrl: "http://www.google.com/search?q=cache:u4IlaMwtlmQJ:www.nofluffjuststuff.com",
+			title: "<b>Ken Sipe</b> - Biography - No Fluff Just Stuff",
+			titleNoFormatting: "Ken Sipe - Biography - No Fluff Just Stuff",
+			content: "<b>Ken</b> has been a practitioner and instructor of RUP since the late 1990s, and an extreme programmer and coach since the middle 2000s. <b>Ken</b> has worked with <b>...</b>"
+		}, {
+			GsearchResultClass: "GwebSearch",
+			unescapedUrl: "http://www.linkedin.com/in/kensipe",
+			url: "http://www.linkedin.com/in/kensipe",
+			visibleUrl: "www.linkedin.com",
+			cacheUrl: "http://www.google.com/search?q=cache:BvP1llzjOQcJ:www.linkedin.com",
+			title: "<b>Ken Sipe</b> | LinkedIn",
+			titleNoFormatting: "Ken Sipe | LinkedIn",
+			content: "View <b>Ken Sipe&#39;s</b> professional profile on LinkedIn. LinkedIn is the world&#39;s largest business network, helping professionals like <b>Ken Sipe</b> discover inside <b>...</b>"
+		}],
+		cursor: {
+			resultCount: "37,100",
+			pages: [{
+				start: "0",
+				label: 1
+			}, {
+				start: "4",
+				label: 2
+			}, {
+				start: "8",
+				label: 3
+			}, {
+				start: "12",
+				label: 4
+			}, {
+				start: "16",
+				label: 5
+			}, {
+				start: "20",
+				label: 6
+			}, {
+				start: "24",
+				label: 7
+			}, {
+				start: "28",
+				label: 8
+			}],
+			estimatedResultCount: "37100",
+			currentPageIndex: 0,
+			moreResultsUrl: "http://www.google.com/search?oe=utf8&ie=utf8&source=uds&start=0&hl=en&q=Ken+Sipe",
+			searchResultTime: "0.23"
+		}
+	},
+	responseDetails: null,
+	responseStatus: 200
+}""" as String
+    }
+}
